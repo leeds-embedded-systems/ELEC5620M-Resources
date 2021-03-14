357c3757643d843940bcaaac370186028efd6300
Tom Carpenter <strange_orange_fish@hotmail.com>
2021-03-14 21:26:01 +0000

    Reduce Indenting in AudioBenchmark.c
    To avoid excess whitespace in the notes.

--- SampleCode/Unit3-1/AudioBenchmark.c
+++ SampleCode/Unit3-1/AudioBenchmark.c
@@ -1,20 +1,18 @@
-        //Always check the FIFO Space before writing or reading left/right channel pointers
-        if ((fifospace_ptr[2] > 0) && (fifospace_ptr[3] > 0)) {
-        
-            ////---- Time Code Execution Between Here ----////
+//Always check the FIFO Space before writing or reading left/right channel pointers
+if ((fifospace_ptr[2] > 0) && (fifospace_ptr[3] > 0)) {
 
-            //If there is space in the write FIFO for both channels:
-            //Increment the phase
-            phase = phase + inc;
-            //Ensure phase is wrapped to range 0 to 2Pi (range of sin function)
-            while (phase >= PI2) {
-                phase = phase - PI2;
-            }
-            // Calculate next sample of the output tone.
-            audio_sample = (signed int)( ampl * sin( phase ) );
-            // Output tone to left and right channels.
-            *audio_left_ptr = audio_sample;
-            *audio_right_ptr = audio_sample;
-
-            ////---- And Here ----////
-        }
+    /******* Time Code Execution Between Here *******/
+    //If there is space in the write FIFO for both channels:
+    //Increment the phase
+    phase = phase + inc;
+    //Ensure phase is wrapped to range 0 to 2Pi (range of sin function)
+    while (phase >= PI2) {
+        phase = phase - PI2;
+    }
+    // Calculate next sample of the output tone.
+    audio_sample = (signed int)( ampl * sin( phase ) );
+    // Output tone to left and right channels.
+    *audio_left_ptr = audio_sample;
+    *audio_right_ptr = audio_sample;
+    /******* And Here *******/
+}

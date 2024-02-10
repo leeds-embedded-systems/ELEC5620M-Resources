4414536f44d07bf0d0b99b725383f2f055f960cf
David Cowell <d.m.j.cowell@leeds.ac.uk>
2024-02-10 16:42:34 +0000

    Add IRQ Header for DE1-SoC
    This provides the Leeds SoC Computer specific IRQ mappings for peripherals.

---
+++ Drivers/DE1SoC_IRQ/DE1SoC_IRQ.h
@@ -0,0 +1,36 @@
+/*
+ * DE1-SoC Customised Variant of the HPS_IRQ Driver
+ * ------------------------------------------------
+ *
+ * Provides named interrupts IDs for the Leeds SoC
+ * computer. Please refer to HPS_IRQ.h for the full
+ * interrupt driver.
+ *
+ * Company: University of Leeds
+ * Author: T Carpenter
+ *
+ * Change Log:
+ *
+ * Date       | Changes
+ * -----------+----------------------------------
+ * 30/01/2024 | Create Header
+ */
+
+#ifndef DE1SOC_IRQ_IDS_H_
+#define DE1SOC_IRQ_IDS_H_
+
+#include "HPS_IRQ/HPS_IRQ.h"
+
+enum {
+    IRQ_INTERVAL_TIMER              = IRQ_FPGA0,
+    IRQ_LSC_KEYS                    = IRQ_FPGA1,
+    IRQ_LSC_AUDIO                   = IRQ_FPGA6,
+    IRQ_LSC_PS2_PRIMARY             = IRQ_FPGA7,
+    IRQ_LSC_JTAG                    = IRQ_FPGA8,
+    IRQ_LSC_IrDA                    = IRQ_FPGA9,
+    IRQ_LSC_GPIO_JP1                = IRQ_FPGA11,
+    IRQ_LSC_GPIO_JP2                = IRQ_FPGA12,
+    IRQ_LSC_PS2_SECONDARY           = IRQ_FPGA17,
+};
+
+#endif /* HPS_IRQ_DE1SOC_IRQ_IDS_H_ */

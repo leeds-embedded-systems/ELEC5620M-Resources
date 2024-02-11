34a833196a5ea7798dbf2a35a6725ebd4090748b
Tom Carpenter <T.Carpenter@leeds.ac.uk>
2024-02-11 17:57:46 +0000

    Update Audio Benchmark Sample
    To account for use of new driver.

--- SampleCode/Unit3-1/AudioBenchmark.c
+++ SampleCode/Unit3-1/AudioBenchmark.c
@@ -1,5 +1,5 @@
 //Always check the FIFO Space before writing or reading left/right channel pointers
-if ((fifospace_ptr[2] > 0) && (fifospace_ptr[3] > 0)) {
+if (space > 0) {
 
     /******* Time Code Execution Between Here *******/
     //If there is space in the write FIFO for both channels:
@@ -12,7 +12,6 @@ if ((fifospace_ptr[2] > 0) && (fifospace_ptr[3] > 0)) {
     // Calculate next sample of the output tone.
     audio_sample = (signed int)( ampl * sin( phase ) );
     // Output tone to left and right channels.
-    *audio_left_ptr = audio_sample;
-    *audio_right_ptr = audio_sample;
+    WM8731_writeSample(audioCtx, audio_sample, audio_sample);
     /******* And Here *******/
 }

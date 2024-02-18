5ad9f751aaacd52c13ab61dd9807ebbc938c6713
Tom Carpenter <T.Carpenter@leeds.ac.uk>
2024-02-18 14:02:10 +0000

    Add Error Code to String Lookup Utility
    You'll have to just trust me that this works (it does). It unfortunately means that the error enumeration has to be declared in a rather weird way in order to be able to generate the string equivalents and lookup tables automatically. But what's the preprocessor for if not crazy stuff like this!
    
    Also add a predefined region for custom error codes in case people wish to add their own. The standard error codes should never be modified as they are shared across multiple projects.

---
+++ Drivers/Util/enum_lookup.h
@@ -0,0 +1,101 @@
+/*
+ * Enum with String Lookup Generator
+ * ---------------------------------
+ *
+ * Allows genenrating typed enumerations using
+ * a macro to define the entries and values. This
+ * same macro can then be used to generate a string
+ * lookup table for the enum values.
+ * 
+ * Company: University of Leeds
+ * Author: T Carpenter
+ *
+ * Change Log:
+ *
+ * Date       | Changes
+ * -----------+----------------------------------
+ * 30/01/2024 | Adapt for embedded code from DLL
+ * 30/01/2018 | Creation of utility
+ *
+ */
+
+#ifndef ENUMLOOKUP_H_
+#define ENUMLOOKUP_H_
+
+#if defined(_WIN32)
+ //Include windows header
+#include <windows.h>
+#elif defined( __linux__ ) || defined(__arm__) || defined(__arm_on_nios__)
+ //Include standard libraries
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#elif defined(__NIOS2__) || defined(__AVR__)
+ //For Nios/Arm HPS/AVR, include the UsefulDefines.h header which has everything we need.
+#include "UsefulDefines.h"
+#include <string.h>
+#else
+#error Unknown System
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Useful defines for Enum/String generation
+#define   GENERATE_ENUM(PFX,NAME,SFX,VAL)   PFX ##  NAME ##  SFX VAL,
+#define GENERATE_STRING(PFX,NAME,SFX,...)  #PFX    #NAME    #SFX,
+#define GENERATE_LOOKUP(PFX,NAME,SFX,...) {#PFX    #NAME    #SFX, (size_t) PFX ## NAME ## SFX},
+
+#define UNKNOWN_STR "???"
+
+typedef struct {
+    const char* str;
+    size_t      enumVal;
+} EnumLookupTable_t;
+
+#if defined(ENUM_LOOKUP_ENABLED) || defined(PCIEUARPLIBSYNC_EXPORTS)
+
+//Convert an enum to string.
+// - Returns NULL if not found
+const char* enumToString(size_t enumVal, const EnumLookupTable_t* lookupTable, size_t lookupTableLength);
+
+//Convert an enum to string.
+// - Retruns `UNKNWONW_STR` if not found
+const char* enumToStringSafe(size_t enumVal, const EnumLookupTable_t* lookupTable, size_t lookupTableLength);
+
+//Convert a string to an enum value
+// - Returns `notFoundVal` if not found.
+size_t stringToEnum(const char* str, const EnumLookupTable_t* lookupTable, size_t lookupTableLength, size_t notFoundValue);
+
+//Pass this to above functions to provide arguments 2 and 3 for a given enum type.
+#define EnumLookupTableAndSize(enumType) enumType##_Lookup, enumType##_Lookup_Length
+
+// Generate lookup table header file
+#define GENERATE_ENUM_LOOKUP_TABLE_HEADER(enumType, lookupMacro) \
+extern const EnumLookupTable_t enumType##_Lookup [];   \
+extern const size_t enumType##_Lookup_Length
+
+// Generate lookup table header file
+#define GENERATE_ENUM_LOOKUP_TABLE_SOURCE(enumType, lookupMacro) \
+const EnumLookupTable_t enumType##_Lookup [] = {  \
+    lookupMacro(GENERATE_LOOKUP,enumType) \
+};                                        \
+const size_t enumType##_Lookup_Length = sizeof(enumType##_Lookup) / sizeof(EnumLookupTable_t)
+
+#else
+// Otherwise don't generate the lookups if not needed.
+#define enumToString(...)     (NULL)
+#define enumToStringSafe(...) UNKNOWN_STR
+#define stringToEnum(...)     (-1)
+
+#define GENERATE_ENUM_LOOKUP_TABLE_HEADER(...)
+#define GENERATE_ENUM_LOOKUP_TABLE_SOURCE(...)
+
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //ENUMLOOKUP_H_
--- Drivers/Util/error.h
+++ Drivers/Util/error.h
@@ -4,6 +4,51 @@
  *
  * Provides a unified set of error codes used by drivers
  *
+ * This file should not be modified as it is shared across
+ * many different projects.
+ * 
+ * 
+ * Custom Error Codes
+ * ------------------
+ * 
+ * To add custom error codes, make your own enumeration and
+ * start them at ERR_CUSTOM_OFF getting more negative. Prefix
+ * any custom error codes with ERR_CUSTOM_ not ERR_ to avoid
+ * future compatibility issues. The largest allowed custom
+ * error code is (ERR_CUSTOM_OFS - 16384) to prevent overflow
+ * on systems which use a 16-bit int. 
+ * 
+ * For example:
+ * 
+ *     enum {
+ *        ERR_CUSTOM_MYERROR = ERR_CUSTOM_OFS,
+ *        ERR_CUSTOM_ANOTHER = ERR_CUSTOM_OFS - 1,
+ *        ... etc ...
+ *     };
+ * 
+ * 
+ * Conversion to String
+ * --------------------
+ * 
+ * To convert to string, use the following which returns
+ * a const char*:
+ * 
+ *     const char* errStr = enumToStringSafe(status, EnumLookupTableAndSize(ErrCodes));
+ * 
+ * This can be piped in to printf, e.g.:
+ * 
+ *     printf("Oh no! Error %s (%d) occurred!", errStr, status);
+ * 
+ * You must globally define ENUM_LOOKUP_ENABLED to use the
+ * lookup feature.
+ * 
+ * Custom error codes do not support char* lookup unless you
+ * make your enum using the macro style used for the main
+ * error enum, and manually check if the value is <= the
+ * custom offset to select your own lookup table.
+ * 
+ * 
+ * 
  * Company: University of Leeds
  * Author: T Carpenter
  *
@@ -11,6 +56,7 @@
  *
  * Date       | Changes
  * -----------+----------------------------------
+ * 18/02/2024 | Add error code string lookup
  * 28/12/2023 | Creation of error code header
  *
  */
@@ -18,14 +64,16 @@
 #ifndef UTIL_ERROR_H_
 #define UTIL_ERROR_H_
 
+#include "enum_lookup.h"
 
 // Default error check scheme:
 typedef signed int HpsErr_t;
 // Success only if ERR_SUCCESS, error otherwise
-#define IS_SUCCESS(code) ((code) == ERR_SUCCESS)
-#define IS_ERROR(code)   !IS_SUCCESS(code)
-#define IS_BUSY(code)    ((code) == ERR_BUSY)
-#define IS_RETRY(code)   ((code) == ERR_AGAIN)
+#define IS_SUCCESS(code)   ((code) == ERR_SUCCESS)
+#define IS_ERROR(code)     !IS_SUCCESS(code)
+#define IS_BUSY(code)      ((code) == ERR_BUSY)
+#define IS_RETRY(code)     ((code) == ERR_AGAIN)
+#define IS_NOSUPPORT(code) ((code) == ERR_NOSUPPORT)
 
 // Extended error check scheme:
 typedef signed int HpsErrExt_t;
@@ -35,54 +83,66 @@ typedef signed int HpsErrExt_t;
 #define IS_TRUE_EXT(code) ((code) > ERR_SUCCESS) // Check if HpsErrExt_t is boolean true
 
 // Error Codes
-enum {
-    // Default success status.
-    ERR_SUCCESS   =  0,
+// This uses a special macro to generate the enum values to enable providing
+// a string lookup of error codes for debug or display purposes.
+// Ensure all lines end with a backslash (\)
+#define ERROR_CODES_LIST(m, type)                                                                      \
+    /* Default success status. */                                                                      \
+    m(,ERR_SUCCESS   ,,=  0)                                                                           \
+                                                                                                       \
+    /* Initialisation failed or not performed */                                                       \
+    m(,ERR_NOINIT    ,,= -1)                                                                           \
+    m(,ERR_PARTIAL   ,,= -2)                                                                           \
+    m(,ERR_UNKNOWN   ,,= -3)                                                                           \
+                                                                                                       \
+    /* Pointers */                                                                                     \
+    m(,ERR_NULLPTR   ,,= -10)                                                                          \
+    m(,ERR_ALIGNMENT ,,= -11)                                                                          \
+    m(,ERR_BADID     ,,= -12)                                                                          \
+                                                                                                       \
+    /* Array access */                                                                                 \
+    m(,ERR_ALLOCFAIL ,,= -20)                                                                          \
+    m(,ERR_NOSPACE   ,,= -21)                                                                          \
+    m(,ERR_BEYONDEND ,,= -22)                                                                          \
+    m(,ERR_NOTFOUND  ,,= -23)                                                                          \
+    m(,ERR_ISEMPTY   ,,= -24)                                                                          \
+    m(,ERR_REVERSED  ,,= -25)                                                                          \
+                                                                                                       \
+    /* Timed out */                                                                                    \
+    m(,ERR_BUSY      ,,= -30)                                                                          \
+    m(,ERR_TIMEOUT   ,,= -31)                                                                          \
+    m(,ERR_ABORTED   ,,= -32)                                                                          \
+    m(,ERR_AGAIN     ,,= -33)  /* Use only if a function must be called again to check some result. */ \
+    m(,ERR_SKIPPED   ,,= -34)                                                                          \
+                                                                                                       \
+    /* Device error */                                                                                 \
+    m(,ERR_BADDEVICE ,,= -40)                                                                          \
+    m(,ERR_NOCONNECT ,,= -41)                                                                          \
+    m(,ERR_INUSE     ,,= -42)                                                                          \
+    m(,ERR_NOSUPPORT ,,= -43)                                                                          \
+    m(,ERR_NOTREADY  ,,= -44)                                                                          \
+    m(,ERR_WRONGMODE ,,= -45)                                                                          \
+                                                                                                       \
+    /* Values */                                                                                       \
+    m(,ERR_TOOBIG    ,,= -50)                                                                          \
+    m(,ERR_TOOSMALL  ,,= -51)                                                                          \
+    m(,ERR_CHECKSUM  ,,= -52)                                                                          \
+    m(,ERR_MISMATCH  ,,= -53)                                                                          \
+    m(,ERR_CORRUPT   ,,= -54)                                                                          \
+    m(,ERR_OUTRANGE  ,,= -55)                                                                          \
+                                                                                                       \
+    /* File/Disk */                                                                                    \
+    m(,ERR_WRITEPROT ,,= -80)                                                                          \
+    m(,ERR_BADDISK   ,,= -81)                                                                          \
+    m(,ERR_IOFAIL    ,,= -82)                                                                          \
+                                                                                                       \
+    /* User Custom Error Codes Offset */                                                               \
+    m(,ERR_CUSTOM_OFS,,= -16384)
 
-    // Initialisation failed or not performed
-    ERR_NOINIT    = -1,
-    ERR_PARTIAL   = -2,
+typedef enum {
+    ERROR_CODES_LIST(GENERATE_ENUM, ErrCodes)
+} ErrCodes;
+GENERATE_ENUM_LOOKUP_TABLE_HEADER(ErrCodes, ERROR_CODES_LIST);
 
-    // Pointers
-    ERR_NULLPTR   = -10,
-    ERR_ALIGNMENT = -11,
-    ERR_BADID     = -12,
-
-    // Array access
-    ERR_ALLOCFAIL = -20,
-    ERR_NOSPACE   = -21,
-    ERR_BEYONDEND = -22,
-    ERR_NOTFOUND  = -23,
-    ERR_ISEMPTY   = -24,
-    ERR_REVERSED  = -25,
-
-    // Timed out
-    ERR_BUSY      = -30,
-    ERR_TIMEOUT   = -31,
-    ERR_ABORTED   = -32,
-    ERR_AGAIN     = -33,  // Use only if a function must be called again to check some result.
-    ERR_SKIPPED   = -34,
-
-    // Device error
-    ERR_BADDEVICE = -40,
-    ERR_NOCONNECT = -41,
-    ERR_INUSE     = -42,
-    ERR_NOSUPPORT = -43,
-    ERR_NOTREADY  = -44,
-    ERR_WRONGMODE = -45,
-
-    // Values
-    ERR_TOOBIG    = -50,
-    ERR_TOOSMALL  = -51,
-    ERR_CHECKSUM  = -52,
-    ERR_MISMATCH  = -53,
-    ERR_CORRUPT   = -54,
-    ERR_OUTRANGE  = -55,
-
-    // File/Disk
-    ERR_WRITEPROT = -80,
-    ERR_BADDISK   = -81,
-    ERR_IOFAIL    = -82
-};
 
 #endif /* UTIL_ERROR_H_ */
